ca76fa41f50c5fd9f6d583ff037ed58df95d6f62
ud15 <61269447+ud15@users.noreply.github.com>
2021-03-23 14:35:58 +0530

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -1,1 +1,31 @@
-
+// JavaScript Document
+$(document).ready(function(){
+	
+	$(".menu").click(function(){
+		//alert($(window).width());
+		if($(window).width() > 480){
+			$("#mySidenav").width(300);
+			$("#main").animate({marginLeft: "300px"});
+			$("#main").animate({opacity: "0.5"},0.2);
+		}
+		else if($(window).width() < 480){
+			$("#mySidenav").width(300);
+			$("#main").animate({marginLeft: "50px"});
+			$("#main").animate({opacity: "0.5"},0.2);
+		}
+	})
+	
+	$(".closebtn").click(function(){
+		//alert($(window).width());
+		if($(window).width() > 480){
+			$("#mySidenav").width(0);
+			$("#main").animate({marginLeft: "0px"});
+			$("#main").animate({opacity: "1"},1);
+		}
+		else if($(window).width() < 480){
+			$("#mySidenav").width(0);
+			$("#main").animate({marginLeft: "0px"});
+			$("#main").animate({opacity: "1"},1);
+		}
+	})
+})
